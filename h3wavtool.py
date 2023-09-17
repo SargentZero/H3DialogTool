9ad404e9e81ffb5782742dbb3c29d7d840f8b682
SargentZero <49167469+SargentZero@users.noreply.github.com>
2023-09-17 01:35:51 -0500

    Quick Update h3wavtool.py
    Added the permutation renaming feature

--- h3wavtool.py
+++ h3wavtool.py
@@ -3,11 +3,18 @@ import re
 import shutil
 
 # Define the directory containing the .wav files. **MAKE SURE YOU USE REPLACE THE SINGLE BACK SLASHES "\" WITH DOUBLE FORWARD SLASHES "//"**
-wav_files_directory = r"C://Program Files (x86)//Steam//steamapps//common//H3EK//data//your_directory_to_your_.wav_files"
+wav_files_directory = r"C://Program Files (x86)//Steam//steamapps//common//H3EK//data//directory_to_your_wav_files"
 
 # Function to extract characters before the bracket from a .wav filename
 def extract_name(filename):
     match = re.search(r'^(.*?)\[\w+\]', filename)
+    if match:
+        return match.group(1)
+    return None
+
+# Function to extract characters within the brackets from a .wav filename
+def extract_characters_in_brackets(filename):
+    match = re.search(r'\[(\w+)\]', filename)
     if match:
         return match.group(1)
     return None
@@ -21,6 +28,7 @@ for root, _, files in os.walk(wav_files_directory):
         if filename.lower().endswith(".wav"):
             wav_path = os.path.join(root, filename)
             name = extract_name(filename)
+            characters_in_brackets = extract_characters_in_brackets(filename)
             if name and len(name) > 0:  # Check if a name was extracted
                 if name not in created_folders:
                     # Create a folder with the extracted name if it doesn't exist
@@ -32,7 +40,14 @@ for root, _, files in os.walk(wav_files_directory):
                 destination_path = os.path.join(destination_folder, filename)
                 shutil.move(wav_path, destination_path)
                 print(f"Moved: {filename} -> {destination_path}")
+
+                # Rename the .wav file to characters within brackets
+                if characters_in_brackets:
+                    new_filename = f"{characters_in_brackets}.wav"
+                    new_wav_path = os.path.join(destination_folder, new_filename)
+                    os.rename(destination_path, new_wav_path)
+                    print(f"Renamed: {filename} -> {new_filename}")
             else:
                 print(f"Skipping: {filename} (Invalid name or no name found. . . Dumbass)")
 
-print("Folder creation and file movement completed. Yeah baby.")
+print("Folder creation, file movement, and renaming completed. Oh yea. . .")
